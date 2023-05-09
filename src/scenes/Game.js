1e33189ce343d48e4008e410dc37f61d62cc1a58
wayungi <fwayungi@gmail.com>
2023-05-10 00:34:03 +0300

    ADD: jumping platform

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,10 +12,12 @@ export default class Game extends Phaser.Scene
     preload()
     {
         this.load.image('background', 'assets/bg_layer1.png')
+        this.load.image('platform', 'assets/ground_grass.png')
     }
 
     create()
     {
         this.add.image(240, 320, 'background')
+        this.add.image(240, 320, 'platform').setScale(0.5)
     }
 }
