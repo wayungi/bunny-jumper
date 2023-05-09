6b35e2a96182ef2edb0a0b9da9f72a8b2bc1a257
wayungi <fwayungi@gmail.com>
2023-05-10 00:13:42 +0300

    ADD: method stubs

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,1 +1,21 @@
 import Phaser from '../lib/phaser.js'
+
+export default class Game extends Phaser.Scene
+{
+
+    constructor()
+    {
+
+
+    }
+
+    preload()
+    {
+
+    }
+
+    create()
+    {
+
+    }
+}
