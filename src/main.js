1bba25e52ee15bc8c0fd43d348939e9b35a7eab9
wayungi <fwayungi@gmail.com>
2023-05-10 00:58:08 +0300

    ADD: bunny as player

--- src/main.js
+++ src/main.js
@@ -5,7 +5,16 @@ export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 480,
     height: 640,
-    scene: Game
+    scene: Game,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+            y: 200
+        },
+            debug: true
+        }
+    }
 })
 
 // console.dir(Phaser)
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -13,11 +13,29 @@ export default class Game extends Phaser.Scene
     {
         this.load.image('background', 'assets/bg_layer1.png')
         this.load.image('platform', 'assets/ground_grass.png')
+        this.load.image('bunny-stand', 'assets/bunny1_stand.png')
     }
 
     create()
     {
         this.add.image(240, 320, 'background')
-        this.add.image(240, 320, 'platform').setScale(0.5)
+        // this.physics.add.image(240, 320, 'platform').setScale(0.5)
+
+        const platforms = this.physics.add.staticGroup()
+
+        for (let i = 0; i < 5; ++i)
+        {
+            const x = Phaser.Math.Between(80, 400)
+            const y = 150 * i
+
+            const platform = platforms.create(x, y, 'platform')
+            platform.scale = 0.5
+
+            const body = platform.body
+            body.updateFromGameObject()
+        }
+        
+        const player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5)
+        this.physics.add.collider(platforms, player)
     }
 }
