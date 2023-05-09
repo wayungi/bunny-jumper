8a52db1c5c9ff7809ad81baf29c4a6311dcb32e9
wayungi <fwayungi@gmail.com>
2023-05-10 00:30:54 +0300

    ADD: game background image

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,17 +5,17 @@ export default class Game extends Phaser.Scene
 
     constructor()
     {
-
+        super('game')
 
     }
 
     preload()
     {
-
+        this.load.image('background', 'assets/bg_layer1.png')
     }
 
     create()
     {
-
+        this.add.image(240, 320, 'background')
     }
 }
