64cf32435d7a674ac985fb6312c9d7816e5296d0
wayungi <fwayungi@gmail.com>
2023-05-10 00:12:05 +0300

    ADD: a game scene file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import Phaser from './lib/phaser.js';
 
-console.dir(Phaser)
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640
+})
+
+// console.dir(Phaser)
 
